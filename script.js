1d2140271c147359f3cca0fd2d58a8d5d20262c7
Danish <dan71ish@gmail.com>
2023-09-21 23:04:28 +0530

    setting functionality

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const startBtn = document.querySelector(".start");
 const timer = document.querySelector(".timer");
 const pause = document.querySelector(".pause");
+const settingOpenBtn = document.querySelector(".setting-btn");
+const settingPanel = document.querySelector(".setting");
+const settingCloseBtn = document.querySelector('.setting-close')
+const breakBtns = document.querySelectorAll('.break');
 const WORK_TIME = 25 * 60;
 const SHORT_BREAK_TIME = 5 * 60;
 const LONG_BREAK_TIME = 15 * 60;
@@ -27,7 +31,7 @@ function updateTime() {
   minute = minute >= 10 ? `${minute}` : `0${minute}`;
   seconds = seconds >= 10 ? `${seconds}` : `0${seconds}`;
   timer.textContent = `${minute}: ${seconds}`;
-  document.title = `${minute}: ${seconds} - Time To Work`;
+  document.title = `${minute}: ${seconds} - Time to Work`;
 }
 
 function startCountdown() {
@@ -41,16 +45,19 @@ function startCountdown() {
         if (cycle === 0) {
           cycle = 1;
           remainingTime = SHORT_BREAK_TIME;
+          addClass(1)
           clearInterval(intervalId);
           LONG_BREAK_COUNT++;
         } else {
           remainingTime = WORK_TIME;
           cycle = 0;
           clearInterval(intervalId);
+          addClass(0)
         }
       } else {
         if (cycle === 0) {
           cycle = 1;
+          addClass(2)
           remainingTime = LONG_BREAK_TIME;
           clearInterval(intervalId);
           LONG_BREAK_COUNT = 0;
@@ -58,10 +65,25 @@ function startCountdown() {
           remainingTime = WORK_TIME;
           cycle = 0;
           clearInterval(intervalId);
+          addClass(0)
         }
       }
       updateTime();
     }
-  }, 1000);
+  }, 1);
 }
 
+settingOpenBtn.addEventListener("click", () => {
+  settingPanel.classList.toggle("hidden");
+});
+settingCloseBtn.addEventListener("click", () => {
+  settingPanel.classList.add("hidden");
+});
+
+
+function addClass(element) {
+  breakBtns.forEach((elem, index) => {
+    elem.classList.remove('active');  
+  });
+  breakBtns[element].classList.add('active')
+}
